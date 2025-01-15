3793bd22ba71c4a9a8270a4a4e9f1579215d2c70
Surve <surve8729@gmail.com>
2025-01-16 00:29:35 +0530

    Add search flight feature

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,25 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all
     @flights = Flight.all
+    if params[:flight]
+      if params[:flight][:departure_airport_id].present?
+        @flights = @flights.where(departure_airport_id: params[:flight][:departure_airport_id])
+      end
+
+      if params[:flight][:arrival_airport_id].present?
+        @flights = @flights.where(arrival_airport_id: params[:flight][:arrival_airport_id])
+      end
+
+      if params[:flight][:start_datetime].present?
+        start_datetime = DateTime.parse(params[:flight][:start_datetime]).utc
+        @flights = @flights.where("start_datetime >= ?", start_datetime)
+      end
+    end
+  end
+
+  # params.require(:recipe).permit(:title, :ingredients, :instructions, :image)
+  def flight_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :start_datetime, :passenger_count)
   end
 end
