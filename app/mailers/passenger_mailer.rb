a8445562fcd5f6342d2ac10d52e75099826dc965
Surve <surve8729@gmail.com>
2025-01-31 20:25:42 +0530

    Setup booking_successful action in mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: "notifications@odinflights.com"
+
+  def booking_successful
+    @user = params[:user]
+    mail(to: @user.email, subject: "Your Flight is successfully booked!")
+  end
 end
